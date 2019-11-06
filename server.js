85da1880d0fb6ea35bbd8886dfb011adb3b3cd3e
Derek Miller <derekmlmt@yahoo.com>
2019-11-06 16:27:12 -0700

    clearing branch after repo clone, and test.

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const routes = "./routes/index.js";
 
-app.use(routes);
+const path = require("path");
+const router = require("express").Router();
 
-
-
-
-
-
-
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
