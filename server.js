ec2b326b10bf54f2397b992cf57fc421581836a7
DerekM84 <49297215+DerekM84@users.noreply.github.com>
2019-11-06 15:09:48 -0700

    Setting up server
    server initialization. switching to mac for installed software.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(routes);
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
